d7736549711b62ff598f79648f044a329a415276
Kiki Huang <kikihuang@teamt5.org>
2023-09-18 12:16:26 +0800

    feat: add logging method

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 import sys
+import logging
 
 if len(sys.argv) != 2:
     print("Usage: python3 main.py <command>")
     print("Available commands: 0(punch-in), 1(punch-out)")
     sys.exit(1)
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='punch-in.log',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 command = sys.argv[1]
 options = webdriver.ChromeOptions()
@@ -18,7 +25,7 @@ options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument("--headless")
 
-chromedriver_path = './chromedriver'  # 将此路径替换为Chromedriver的实际路径
+chromedriver_path = './chromedriver'
 service = webdriver.chrome.service.Service(chromedriver_path)
 
 driver = webdriver.Chrome(options=options)
@@ -44,18 +51,23 @@ else:
     print("Unknown command:", command)
 
 try:
+    print("Logging in...")
     driver.switch_to.frame("PageContent_FrameContent")
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, btn_xpth))
     )
     element.click()
-
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}", exc_info=True)
+    print(f"An error occurred: {str(e)}")
+    sys.exit(1)
 finally:
     time.sleep(2)
 
 driver.switch_to.default_content()
 
 try:
+    print("Punching in/out...")
     WebDriverWait(driver, 10).until(
         EC.frame_to_be_available_and_switch_to_it((By.ID, "popupHelper_CIF-1")))
     popup_window = WebDriverWait(driver, 10).until(
@@ -64,11 +76,17 @@ try:
     )
     popup_text = popup_window.text
     file_name = "punch-in.log"
+    print(popup_text)
     with open(file_name, "a", encoding="utf-8") as file:
         file.write(popup_text + "\n")
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}", exc_info=True)
+    print(f"An error occurred: {str(e)}")
+    sys.exit(1)
 finally:
     time.sleep(2)
 
 driver.switch_to.default_content()
 
 driver.quit()
+print("Done.")
